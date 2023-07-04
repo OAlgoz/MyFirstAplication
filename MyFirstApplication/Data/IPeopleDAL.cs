bec6426a29031f36fd7193b7d0e780d533753a31
Thiago <thiagomassulaalmeida@gmail.com>
2023-07-04 20:22:04 -0300

    initial

---
+++ MyFirstApplication/Data/IPeopleDAL.cs
@@ -0,0 +1,13 @@
+﻿using MyFirstApplication.Model;
+
+namespace MyFirstApplication.Data
+{
+    public interface IPeopleDAL
+    {
+        Task Create(People entity);
+        Task Delete(int id);
+        Task<People> Get(int id);
+        Task<IEnumerable<People>> GetAll();
+        Task Update(People entity);
+    }
+}
